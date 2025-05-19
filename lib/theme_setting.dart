8b6650757f9a9ed764ef39a95d8218fcebf8ac57
SuraiyaKhatunItHome <suraiya.it.home@gmail.com>
2025-05-19 20:05:14 +0600

    example File Update

--- lib/theme_setting.dart
+++ lib/theme_setting.dart
@@ -1,16 +1,10 @@
 library theme_setting;
 
+export 'config/bloc_provider/register_theme_setting_bloc_provider.dart';
 export 'config/di/theme_setting_get_it_register.dart';
 export 'enum/supported_theme_name.dart';
+export 'extension/brightness_extension.dart';
 export 'utils/json_color_util.dart';
-export 'view_models/dark_mode_cubit/dark_mode_cubit.dart';
 export 'view_models/dark_mode_hydrated_cubit/dark_mode_hydrated_cubit.dart';
-export 'view_models/theme_bloc/bloc.dart';
-export 'view_models/theme_bloc/theme_bloc.dart';
-export 'view_models/theme_bloc/theme_event.dart';
-export 'view_models/theme_bloc/theme_state.dart';
-export 'view_models/toggle_dark_mode/toggle_dark_mode_cubit.dart';
-export 'view_models/toggle_dark_mode/toggle_dark_mode_state.dart';
-export 'views/screens/theme_setting.dart';
-export 'views/widgets/theme_switch_cubit_impl.dart';
 export 'views/widgets/theme_switch_toggle_button.dart';
+
